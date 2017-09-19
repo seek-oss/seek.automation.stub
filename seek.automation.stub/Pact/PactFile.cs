45b15245d0e2405e58dad2e3708d7b3d976c8f89
b3hdad <b3hdad@outlook.com>
2017-09-19 23:08:36 +1000

    Updated so that it works with Pact.net 2+

---
+++ seek.automation.stub/Pact/PactFile.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace PactNet.Models
+{
+    public class PactFile : PactDetails
+    {
+        [JsonProperty(PropertyName = "metadata")]
+        public dynamic Metadata { get; private set; }
+
+        public PactFile()
+        {
+            Metadata = new
+            {
+                pactSpecificationVersion = "1.1.0"
+            };
+        }
+    }
+}
